337d066fd5ef95ff69cbbea7db4df231869e588e
davidhickey <davidhickey423@gmail.com>
2018-12-17 16:28:37 -0500

    builds child components for MortyQuote component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import MortyQuote from './components/MortyQuote';
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <div>
             Edit <code>src/App.js</code> and save to reload.
             <MortyQuote />
-          </p>
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/components/MortyQuote.js
+++ src/components/MortyQuote.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react';
 
 class MortyQuote extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     error: null,
+     isLoaded: false,
+     items: []
+   };
+ }
   render() {
     return (
-    <p>MortyQuote here</p>
-    );
+      <div>
+        <div>
+          <QuoteInput />
+        </div>
+        <div>
+          <QuoteTable />
+        </div>
+      </div>
+
+  );
   }
 }
 
+
+class QuoteInput extends Component {
+  render() {
+    return (<p>QuoteInput here</p>)
+  }
+}
+class QuoteTable extends Component {
+  render() {
+    return (
+      <div>
+        <p>QuoteTable here</p>
+        <div>
+          <div>
+            <FilterQuotes />
+          </div>
+          <div>
+            <SortQuotes />
+          </div>
+          <div>
+          <DataQuotes />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class FilterQuotes extends Component {
+  render() {
+    return (<span>FilterQuotes here</span>)
+  }
+}
+
+class SortQuotes extends Component {
+  render() {
+    return (<span>SortQuotes here</span>)
+  }
+}
+
+class DataQuotes extends Component {
+  render() {
+    return (<table>DataQuotes here</table>)
+  }
+}
 
 export default MortyQuote;
