bc5e5c1a61b51ea8d1f8027062a8ebab0f7f131e
davidhickey <davidhickey423@gmail.com>
2018-12-19 14:24:46 -0500

    renders api data in react-table

--- src/components/MortyQuote.js
+++ src/components/MortyQuote.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Form, FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 class MortyQuote extends Component {
   constructor(props) {
    super(props);
@@ -30,12 +31,10 @@ class MortyQuote extends Component {
 class QuoteInput extends Component {
   constructor(props, context) {
     super(props, context);
-
-    this.handleChange = this.handleChange.bind(this);
-
     this.state = {
       value: ''
     };
+    this.handleChange = this.handleChange.bind(this);
   }
   getValidationState() {
     const length = this.state.value.length;
@@ -71,9 +70,52 @@ class QuoteInput extends Component {
   }
 }
 
-
 class QuoteTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: null,
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  componentDidMount () {
+    this.setState({isLoading: true});
+
+    var url = 'http://morty.mockable.io/quotes?&loan_amount=100000'
+    fetch(url)
+    // .then(response => response.json())
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong ...');
+      }
+    })
+    .then(data => {
+      console.log(data)
+      this.setState({
+        quotes: data,
+        isLoading: false
+      });
+
+    })
+    .catch(error => this.setState({
+      error,
+      isLoading: false
+    })
+  );
+  }
+
   render() {
+    const { hits, isLoading, error } = this.state;
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+    if(isLoading){
+      return <p>Quotes on the way</p>;
+    }
     return (
       <div>
         <p>QuoteTable here</p>
@@ -85,12 +127,13 @@ class QuoteTable extends Component {
             <SortQuotes />
           </div>
           <div>
-          <DataQuotes />
+          <DataQuotes {...this.props} {...this.state} />
           </div>
         </div>
       </div>
     )
   }
+
 }
 
 class FilterQuotes extends Component {
@@ -106,8 +149,23 @@ class SortQuotes extends Component {
 }
 
 class DataQuotes extends Component {
+
   render() {
-    return (<table>DataQuotes here</table>)
+    // if(this.props.quotes.length == 0) { return null; }
+
+    return (
+      <BootstrapTable data={this.props.quotes}>
+      <TableHeaderColumn isKey dataField="id">Quote#</TableHeaderColumn>
+      <TableHeaderColumn dataField="lender.name">Lender</TableHeaderColumn>
+      <TableHeaderColumn dataField="loan_product">Loan Product</TableHeaderColumn>
+      <TableHeaderColumn dataField="interest_rate">Interest Rate</TableHeaderColumn>
+      <TableHeaderColumn dataField="loan_term">Loan Term</TableHeaderColumn>
+      <TableHeaderColumn dataField="monthly_payment">Monthly Payment</TableHeaderColumn>
+      <TableHeaderColumn dataField="rate_type">Rate Type</TableHeaderColumn>
+
+      </BootstrapTable>
+
+    )
   }
 }
 
